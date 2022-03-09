26afe5a24b33f960e4ce99e4acaa15cd1116648c
Pablo <delatorre.pablodaniel@gmail.com>
2022-03-08 23:12:19 -0300

    Estilo calculadora terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='titulo'>Calculadora con React</h1>
+      <div className='calculator-grid'>
+        <div className='output'>
+          <div className='valor-previo'></div>
+          <div className='valor-actual'></div>
+        </div>
+        <button className='doble-largo'>AC</button>
+        <button>DEL</button>
+        <button>/</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button className='doble-largo'>=</button>
+      </div>
     </div>
   );
 }
